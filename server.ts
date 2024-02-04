ffb1b8fc4aede48e7fde067b93286d4a55f2b9d4
Nikhil Mehta <nikhil.001mehta@gmail.com>
2024-02-04 22:05:43 +0530

    refactor: using nextjs docs like custom server

--- server.ts
+++ server.ts
@@ -1,48 +1,39 @@
-import type { NextFunction, Request, Response } from "express";
-import express from "express";
 import http from "http";
 import next from "next";
-import type { NextServer } from "next/dist/server/next";
+import { parse } from "url";
 
 const PORT = 3000;
 
-export function staticRequestChecker(
-  app: NextServer & { hasStaticDir?: boolean },
-  basePath = ""
-) {
-  const { hasStaticDir } = app;
-  if (basePath && !basePath.startsWith("/")) basePath = `/${basePath}`;
-  const nextRegex = new RegExp(`^${basePath}/_next/(?!data)`);
-  const staticRegex = hasStaticDir && new RegExp(`^${basePath}/_static/`);
-  return (req: Request) => {
-    const url = req.url || "";
-    return url.match(nextRegex) || (staticRegex && url.match(staticRegex));
-  };
+async function main() {
+  const app = next({
+    dev: true,
+    hostname: "localhost",
+    port: PORT,
+  });
+  const handle = app.getRequestHandler();
+  await app.prepare();
+  const server = http.createServer(async (req: any, res: any) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      if (parsedUrl.pathname === "/route1" && req.method === "GET") {
+        res.setHeader("set-cookie", "GET_COOKIE=GET_COOKIE");
+      }
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      res.statusCode = 500;
+      res.end("Internal Server Error");
+    }
+  });
+  server.once("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  server.listen(PORT, () => {
+    console.log(`- Local: http://localhost:${PORT}`);
+  });
 }
 
-const nextApp = next({
-  dev: true,
-  hostname: "localhost",
-  port: PORT,
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
-const exapp = express();
-const isStaticRequest = staticRequestChecker(nextApp, "/");
-exapp.use((req: Request, res: Response, next: NextFunction) => {
-  let nextReqHandler = nextApp.getRequestHandler();
-  if (isStaticRequest(req)) {
-    return nextReqHandler(req, res);
-  }
-  return next();
-});
-exapp.all("*", (req, res) => {
-  if (req.originalUrl.includes("/route1") && req.method === "GET") {
-    // if you do not set this cookie then server_action_cookie is set correctly on UA
-    // but if you set this cookie here then this overrides server_action_cookie
-    res.cookie("GET_COOKIE", "GET_COOKIE");
-  }
-  let nextReqHandler = nextApp.getRequestHandler();
-  return nextReqHandler(req, res);
-});
-const server = http.createServer(exapp);
-nextApp.prepare();
-server.listen(PORT);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ async function redirectToRoute1() {
 
 export default function page() {
     return <form action={redirectToRoute1} className="prose dark:text-gray-200 text-gray-800">
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redirect me to /route1</button>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redirect me to /route1</button>
         <ul>
             <strong className="dark:text-gray-200 text-gray-800">Expected Result</strong>
             <li>After sucessful redirection with streaming both cookies should be dropped on browser</li>
@@ -22,8 +22,8 @@ export default function page() {
                     if you comment out {" "}
                     <a
                         className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                        href="https://github.com/technikhil314/nextjs-parallel-routes-sample/blob/chore/steaming-cookie-issue/server.ts#L41">
-                        line 41 {" "}
+                        href="https://github.com/technikhil314/nextjs-parallel-routes-sample/blob/chore/steaming-cookie-issue/server.ts#L19">
+                        line 19 {" "}
                     </a> in server.ts file then
                     <br />
                     Server_Action_Cookie is dropped correctly on browser
@@ -32,8 +32,8 @@ export default function page() {
                     if you uncomment {" "}
                     <a
                         className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                        href="https://github.com/technikhil314/nextjs-parallel-routes-sample/blob/chore/steaming-cookie-issue/server.ts#L41">
-                        line 41 {" "}
+                        href="https://github.com/technikhil314/nextjs-parallel-routes-sample/blob/chore/steaming-cookie-issue/server.ts#L19">
+                        line 19 {" "}
                     </a>
                     in server.ts file then
                     <br />
